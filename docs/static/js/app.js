fbc839539ceef1ba276a7c0d73c37a11c7142e47
ssumanss <ssuman44@gmail.com>
2019-06-01 17:43:36 +0530

    change made.

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/sw.js')
+           .register('/abstract-algebra/sw.js')
            .then(function() { console.log("Service Worker Registered"); });
 }
